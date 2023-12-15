a2fffc31e3ea710880abb421915e5c2c373ce63d
Mich <20213tn011@utez.edu.mx>
2023-12-15 06:00:48 -0600

    Shut Down

--- app.py
+++ app.py
@@ -1,73 +1,76 @@
+# Importar las bibliotecas necesarias
 from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO
 from method import methodCesar, decryptCesar
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SECRET_KEY'] = 'secret_key'  # Clave secreta para manejar sesiones de usuario
 socketio = SocketIO(app)
 
-#Llamada front 
+# Definir la ruta principal que renderiza el archivo 'main.html'
 @app.route('/')
 def main():
-    #Buscando archivo para renderizar  
     return render_template('main.html')
 
-#---------------------------
-#Función que se ejcutará 
+# Ruta para gestionar la entrada al chat
 @app.route('/chatOnline', methods=['POST'])
 def chatOnline():
-    #Accede a parámetros enviados
-    #Mediante el valor del campo 
     nameUser = request.form['nameUser']
-    #Asignación de la variable 
     typeEncryption = request.form['typeEncryption']
     
-    #Almacena valores en el objeto session 
+    # Almacenar valores en el objeto de sesión para mantener la información del usuario
     session['nameUser'] = nameUser
     session['typeEncryption'] = typeEncryption
-    #Futuras solicitudes
     
+    # Renderizar la plantilla 'chat.html' con la información del usuario
     return render_template('chat.html', nameUser=nameUser, typeEncryption=typeEncryption)
 
-#---------------------------
+# En el servidor, en la función receive
 @socketio.on('message')
 def receive(data):
     nameUser = session.get('nameUser', 'Invitado')
+    clave = int(session.get('typeEncryption', '0'))
 
-    clave = int(session.get('typeEncryption', '0'))  # Obtener la clave del tipo de cifrado
+    # Aplicar cifrado César al mensaje recibido
     messageEncryption = methodCesar(data['message'], clave)
 
     print(f'Received message from {nameUser}: {data["message"]}')
     print(f'Mensaje cifrado: {messageEncryption}')
 
-    # Emitir el mensaje cifrado y descifrado
-    socketio.emit('message', {'cifrado': f'Cifrado: {messageEncryption}', 'descifrado': f'decifrada: {data["message"]}'})
+    # Emitir el mensaje cifrado y descifrado al destinatario correcto
+    destination_chat = 2 if data['chatNumber'] == 1 else 1
 
-#----------------------------
+    socketio.emit('message', {
+        'cifrado': f'Cifrado: {messageEncryption}',
+        'descifrado': f'Descifrado: {data["message"]}',
+        'chatNumber': destination_chat
+    })
+
+
+# En el servidor, en la función descifrar
 @socketio.on('descifrar')
 def descifrar(data):
     nameUser = session.get('nameUser', 'Invitado')
 
-    # Obtén el mensaje cifrado y la clave de descifrado
-    mensaje_completo = data.get('mensajeCifrado', '')
+    mensaje_cifrado = data.get('mensajeCifrado', '')
     clave_descifrado = int(data.get('claveDescifrado', 0))
-
-    # Extraer el mensaje cifrado de la parte "Usuario: Mensaje cifrado"
-    _, mensaje_cifrado = mensaje_completo.split(': ', 1)  # Divide en dos partes, solo 1 vez
+    chatNumber = data.get('chatNumber', 0)
 
     try:
-        # Descifrar el mensaje
         mensaje_descifrado = decryptCesar(mensaje_cifrado, clave_descifrado)
-        mensaje_resultado = f'decifrada: {mensaje_descifrado}'
+        mensaje_resultado = f'Descifrado: {mensaje_descifrado}'
     except ValueError:
-        # La clave es incorrecta, mostrar la palabra sin cambios
-        mensaje_resultado = f'aun no decifrada: {mensaje_cifrado}'
+        mensaje_resultado = f'Aun no descifrada: {mensaje_cifrado}'
 
     print(f'Descifrado del mensaje de {nameUser}: {mensaje_resultado}')
 
-    # Emitir el mensaje descifrado
-    socketio.emit('descifrado', {'descifrado': f'{mensaje_resultado}'})
+    # Emitir el mensaje descifrado al destinatario correcto
+    socketio.emit('descifrado', {
+        'descifrado': mensaje_resultado,
+        'chatNumber': chatNumber
+    })
 
+# Ejecutar la aplicación con SocketIO en el puerto predeterminado
 if __name__ == '__main__':
-    #Ejecutar la apliacion en el puerto predeterminado por Socketio
     socketio.run(app)
